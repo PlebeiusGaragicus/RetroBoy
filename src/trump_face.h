45f698bab51b5da82e878edb84049cfd95181b9b
PlebbyG <plebeiusgaragicus@gmail.com>
2024-08-24 17:05:21 -0700

    splash

---
+++ src/trump_face.h
@@ -0,0 +1,11 @@
+// Generated using GB Export by AngelSix https://github.com/angelsix/retrosix-resources
+// Enhanced version based of original by https://github.com/gabrielcnr
+
+#define /Users/satoshi/Downloads/TrumpFace_tilemap_width 0
+#define /Users/satoshi/Downloads/TrumpFace_tilemap_height 0
+
+#define /Users/satoshi/Downloads/TrumpFace_tileset_size 202
+
+extern const unsigned char /Users/satoshi/Downloads/TrumpFace_tileset[]; 
+
+extern const unsigned char /Users/satoshi/Downloads/TrumpFace_tilemap[];
