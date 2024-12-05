9602e46c7f255922e29d25c4ae0289a9b4ee308d
PlebbyG <plebeiusgaragicus@gmail.com>
2024-12-04 19:17:26 -0800

    works!

--- src/main.c
+++ src/main.c
@@ -1,51 +1,99 @@
 #include <gb/gb.h>
 #include <gb/drawing.h>
 
-uint8_t x = 3, y = 6, i = 3, j, f;
+#include <stdint.h>
+#include <stdio.h>
+#include <rand.h>
 
-void main() {
-	color(DKGREY, WHITE, SOLID); //  forecolor, backcolor, mode [Colors: WHITE (0), LTGREY (1), DKGREY(2), BLACK (3)]
-	// circle(100, 60, 30, M_FILL); // x, y, radius, style (M_FILL or M_NOFILL)
-	// line(40, 40, 50, 50); // x1, y1, x2, y2
-	// box(120, 125, 125, 135, M_NOFILL); // x1, y1, x2, y2, style (M_FILL or M_NOFILL)
 
-	// plot_point(3, 4); // Plot a single pixel on the screen
-	// gotogxy(18, 15); // Places you at these coordinates
-	// gprintf("asdf"); // When using the drawing library you must use gprintf instead of printf
+uint8_t PlayerX = 0;
+uint8_t PlayerY = 0;
+
+
+unsigned char CrossSprite[] =
+{
+    0x00,0x00,0x00,0x00,0x10,0x10,0x38,0x38,0x10,0x10,0x00,0x00,0x00,0x00,0x00,0x00,
+};
+
+
+
+int random(int min, int max) {
+	return (rand() % (max - min + 1)) + min;
+}
+
+
+
+void init()
+{
+	SHOW_BKG;
+	SHOW_SPRITES;
+	DISPLAY_ON;
+}
+
+
+void seed_prng()
+{
+        printf(" \n\n\n\n\n\n\n\n    PRESS START!\n");
+        // abuse user input for seed generation
+        waitpad(J_START);
+        uint16_t seed = LY_REG;
+        seed |= (uint16_t)DIV_REG << 8;
+        initrand(seed);
+
+		printf("\n\nLucky number: %d\n\n", seed);
+		delay(250);
+		// clear_screen();
+
+		uint8_t countdown = 20;
+        while(countdown-- > 0)
+        {
+            uint8_t r = ((uint8_t)rand()) % (uint8_t)2;
+            printf("%d", r);
+
+			vsync();
+        }
+}
+
+
+void clear_screen() {
+	uint8_t j, f;
+	for (j = 0; j < 20; j++) { // GB screen is 20 columns wide and 18 columns tall
+		for (f = 0; f < 18; f++) { 
+			gotogxy(j, f);
+			wrtchr(' '); // Use wrtchr to place a character when using the drawing library
+		}
+	}
+}
+
+
+void main()
+{
+	init();
+	seed_prng();
+	clear_screen();
+
+	set_sprite_data(0, 16, CrossSprite);
+	color(DKGREY, WHITE, SOLID);
+
+
+	PlayerX = random(10, 190);
+	PlayerY = random(10, 190);
 
 	uint8_t key;
 	while (1) {
 		key = joypad();
 
-		// print("X: %d, Y: %d, Color: %d\n", x, y, i);
-		// print(key);
-
 		if (key & J_UP)
-			y--;
-		if (key & J_DOWN)
-			y++;
-		if (key & J_LEFT)
-			x--;
-		if (key & J_RIGHT)
-			x++;
-	
-		// if (key == J_A) { // Change colors
-		// 	waitpadup();
-		// 	i++;
-		// 	if (i > 3)
-		// 		i = 0;
-		// }
+			PlayerY--;
+		else if (key & J_DOWN)
+			PlayerY++;
 
-		// if (key == J_B) { // Function to 'clear' the screen
-		// 	for (j = 0; j < 20; j++) { // GB screen is 20 columns wide and 18 columns tall
-		// 		for (f = 0; f < 18; f++) { 
-		// 			gotogxy(j, f);
-		// 			wrtchr(' '); // Use wrtchr to place a character when using the drawing library
-		// 		}
-		// 	}
-		// }
-		plot(x, y, i, SOLID); // Draws a pixel on the screen (x, y, color, mode)
-		// delay(10);
+		if (key & J_LEFT)
+			PlayerX--;
+		else if (key & J_RIGHT)
+			PlayerX++;
+
+		move_sprite(0, PlayerX, PlayerY);
 
 		vsync();
 	}
